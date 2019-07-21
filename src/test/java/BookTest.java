5cc71efa8c04d61c45bf2d9b7511e3496cde007f
sujin <tnwls9712@ajou.ac.kr>
2019-07-21 17:27:52 +0900

    Add 북리스트사이즈Test

--- src/test/java/BookTest.java
+++ src/test/java/BookTest.java
@@ -1,5 +1,21 @@
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
 
 public class BookTest{
+
+    @Test //황수진 method 1
+    public void 북리스트사이즈테스트(){
+        List<Book> books =new ArrayList<Book>();
+        books.add(new Book(1234,"HarryPotter 1","J.K"));
+        books.add(new Book(1235,"HarryPotter 2","J.K"));
+        books.add(new Book(1236,"HarryPotter3 ","J.K"));
+        assertThat(books.size(),is(3));
+    }
 
 
 }
