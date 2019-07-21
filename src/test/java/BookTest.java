04978c9b8f3a1281a98e0c05fff2b83aefb964a5
seungjae95 <52422276+seungjae95@users.noreply.github.com>
2019-07-21 17:57:36 +0900

    이승재 3개 테스트코드 업데이트
    테스트코드 3개 업데이트 완료

--- src/test/java/BookTest.java
+++ src/test/java/BookTest.java
@@ -3,30 +3,35 @@ import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 
-import java.util.List;
-
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Matchers.anyString;
 import static org.mockito.Mockito.*;
+
 @RunWith(MockitoJUnitRunner.class)
-public class BookTest {
+public class BookTest{
+
     @Mock
-    BookManagement bookManagement;
+    private BookManagement bookManagement;
 
-
-    @Test(expected = IllegalArgumentException.class)
-    public void 책번호를_음수로_저장하면_예외발생(){
-        Book book=mock(Book.class);
-        doThrow(new IllegalArgumentException()).when(book).setNum(-1);
-        book.setNum(-1);
+    @Test
+    public void 책_작가_이름_저장(){
+        Book book = mock(Book.class);
+        book.setAuthor("베르나르 베르베르");
+        verify(book).setAuthor(anyString());
+    }
+    @Test
+    public void 책_번호가_맞는지(){
+        given(bookManagement.findBook(2)).willReturn(new Book(2, "어린왕자", "생택쥐베리"));
+        Book book = bookManagement.findBook(2);
+        assertThat(book.getNum(), is(2));
     }
 
     @Test
-    public void 빈객체인지테스트(){
-        List<Book>bookList=mock(List.class);
-assertThat(bookList.size(),is(0));
+    public void 책번호로찾기_적어도_한번_실행되는지() {
+        given(bookManagement.findBook(1)).willReturn(new Book(1, "개미", "베르나르 베르베르"));
+        Book book = bookManagement.findBook(1);
+        verify(bookManagement, atLeast(1)).findBook(1);
     }
-
 }
